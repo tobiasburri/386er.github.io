e177aba2f9df4d08b789d44c8b4e980444b8d298
security4cloud <t.burri@reply.de>
2021-01-06 23:54:47 +0100

    add star theme

--- shopmvpdev/templates/categoriesTemplate.js
+++ shopmvpdev/templates/categoriesTemplate.js
@@ -12,7 +12,7 @@ var categoriesTemplate =`
   <div class="selector skull" data-value="skull" onclick="" style="cursor: pointer;">	<img class="selected-image" src="./images/skull.svg"></div>
   <div class="selector diamond" data-value="diamond" onclick="" style="cursor: pointer;">	<img class="selected-image" src="./images/diamond.svg"></div>
   <div class="selector brain" data-value="brain" onclick="" style="cursor: pointer;">	<img class="selected-image" src="./images/brain.svg"></div>
-  <div class="selector diamond" data-value="diamond" onclick="" style="cursor: pointer;">	<img class="selected-image" src="./images/diamond.svg"></div>
+  <div class="selector star" data-value="star" onclick="" style="cursor: pointer;">	<img class="selected-image" src="./images/star.svg"></div>
 </div>
 
 	<button aria-label="Previous" class="glider-prev"><img class="select-button" src="./images/left.svg"></button>
